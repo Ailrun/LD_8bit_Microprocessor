c4af1cfb7c5baf75c96ef7dd0dca13cc862b0d15
Ailrun <jjc9310@gmail.com>
2015-06-03 07:46:22 +0900

    No connect'n error but too big becuz of DMem

--- PCCounter.v
+++ PCCounter.v
@@ -17,7 +17,7 @@ module PCCounter
 
    assign pcAddress = pc;
    assign flags[1] = OF1 | OF2;
-   assign flags[0] = (sigBranch && !(W2 > pc));
+   assign flags[0] = (sigBranch && adding[1] == 1'b1)?1'b1:1'b0;
 
    CLA_8bit cla8_0(.A(pc), .B(8'b0000_0001), .Ci(1'b0), .S(W1), .OF(OF1));
    CLA_8bit cla8_1(.A(W1), .B(adding), .Ci(1'b0), .S(W2), .OF(OF2));
